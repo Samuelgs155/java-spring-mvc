2ef22c93f6020f18b0b8ac6e94f77ea20c09bfa2
Samuelgs155 <samuelgs15@hotmail.com>
2021-02-08 13:25:45 +0100

    Add files via upload

--- backendninja/src/main/java/com/spring/example/controller/ExampleController6Post.java
+++ backendninja/src/main/java/com/spring/example/controller/ExampleController6Post.java
@@ -1,5 +1,7 @@
 package com.spring.example.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,19 +9,39 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.spring.example.model.Person;
 
 @Controller
 @RequestMapping("/example6")
 public class ExampleController6Post {
+	
+	private static final Log LOGGER = LogFactory.getLog(ExampleController6Post.class);
 
 	public static final String FORM_VIEW = "form";
 	public static final String RESULT_VIEW = "result";
+	
+	// Forma 1 redireccionar
+//	@GetMapping("/")
+//	public String redirect() {
+//		return "redirect:/example6/showform";
+//	}
+	
+	// Forma 2 redireccionar
+	@GetMapping("/")
+	public RedirectView redirect() {
+		return new RedirectView("/example6/showform");
+	}
 
 	@GetMapping("/showform")
 	public String showForm(Model model) {
+		LOGGER.info("INFO TRACE");
+		LOGGER.warn("Warning TRACE");
+		LOGGER.error("Error TRACE");
+		LOGGER.debug("Debug TRACE");
 		model.addAttribute("person", new Person());
+		// int i = 6 / 0;
 		return FORM_VIEW;
 	}
 	
